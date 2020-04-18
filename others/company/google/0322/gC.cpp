041176b5ead154e6acdc26178955c4b7289079c7
dcsgman <rongliangfu@outlook.com>
2020-04-18 21:56:34 +0800

    icode

--- others/company/google/0322/gC.cpp
+++ others/company/google/0322/gC.cpp
@@ -1,52 +1,117 @@
 #include <iostream>
-#include <vector>
+#include <queue>
 #include <cmath>
 #include <algorithm>
 using namespace std;
 
-// 二分答案
-
-int maxV;
 int T, N, K;
+
+int fun_(vector<int> sessions)
+{
+    sort(sessions.begin(), sessions.end(), greater<int>());
+    while (K--)
+    {
+        while (!sessions.empty() && sessions.back() == 1)
+            sessions.pop_back();
+        if (sessions.empty())
+            break;
+        int s0 = sessions[0] / 2;
+        int s1 = s0;
+        if (sessions[0] % 2 == 1)
+        {
+            ++s1;
+        }
+        sessions[0] = s1;
+        sessions.push_back(s0);
+        sort(sessions.begin(), sessions.end(), greater<int>());
+    }
+    int maxV = 1;
+    if (!sessions.empty())
+    {
+        maxV = sessions[0];
+    }
+    return maxV;
+}
+
+int fun_pq(vector<int> sessions)
+{
+    priority_queue<int, vector<int>, less<int>> pq;
+    for (auto it:sessions)
+    {
+        if (it > 1)
+            pq.push(it);
+    }
+
+    while (K--)
+    {
+        if (pq.empty())
+            break;
+        int s0 = pq.top() / 2;
+        int s1 = s0;
+        if (pq.top() % 2 == 1)
+        {
+            ++s1;
+        }
+        pq.pop();
+        if (s0 > 1)
+            pq.push(s0);
+        if (s1 > 1)
+            pq.push(s1);
+    }
+    int maxV = 1;
+    if (!pq.empty())
+    {
+        maxV = pq.top();
+    }
+    return maxV;
+}
+
+int check(int mid, vector<int> sessions)
+{
+    int num = 0;
+    for (auto it:sessions)
+    {
+        if (it > mid)
+            num += ceil(double(it) / mid)-1;
+    }
+    return num;
+}
+
+// Dichotomous answer
+int fun_da(vector<int> sessions, int maxS)
+{
+    int left = 1, right = maxS;
+    while (left <= right)
+    {
+        int mid = (right + left) >> 1, num = check(mid, sessions);
+        if (num > K)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+    return left;
+}
 
 int main()
 {
-    int i, j, index = 1;
+    int index = 1;
     cin >> T;
     while (T--)
     {
         cin >> N >> K;
-        int tp = 0;
-        vector<int> sessions(N, 0);
-        cin >> sessions[0];
-        for (i = 1; i < N; ++i)
+        int bef = 0, next = 0, maxS=0;
+        vector<int> sessions;
+        cin >> bef;
+        for (int i = 1; i < N; ++i)
         {
-            cin >> sessions[i];
-            sessions[i - 1] = sessions[i] - sessions[i - 1];
+            cin >> next;
+            bef = next - bef;
+            maxS = max(maxS, bef);
+            if (bef > 1)
+                sessions.push_back(bef);
+            bef = next;
         }
-        sessions.pop_back();
-        sort(sessions.begin(), sessions.end(), greater<int>());
-        maxV = 1;
-        while (K--)
-        {
-            while (!sessions.empty() && sessions.back() == 1)
-                sessions.pop_back();
-            if (sessions.empty())
-                break;
-            int s0 = sessions[0] / 2;
-            int s1 = s0;
-            if (sessions[0] % 2 == 1)
-            {
-                ++s1;
-            }
-            sessions[0] = s1;
-            sessions.push_back(s0);
-            sort(sessions.begin(), sessions.end(), greater<int>());
-        }
-        if (!sessions.empty()) {
-            maxV = sessions[0];
-        }
-        cout << "Case #" << (index++) << ": " << maxV << endl;
+        cout << "Case #" << (index++) << ": " << fun_da(sessions, maxS) << endl;
     }
     return 0;
 }
--- others/company/google/0322/gD.cpp
+++ others/company/google/0322/gD.cpp
@@ -1,37 +1,55 @@
-#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
 #include <vector>
 #include <cmath>
-#include <algorithm>
+#include <map>
+#include <queue>
+#include <string>
+#include <iostream>
+
 using namespace std;
-// 贪心
-int maxV;
-int T, N, K;
 
-int main()
-{
-    int i, j, index = 1;
-    cin >> T;
-    while (T--)
-    {
-        cin >> N >> K;
-        int tp = 0;
-        vector<string> sessions(N);
-        for (i = 1; i < N; ++i)
-        {
-            cin >> sessions[i];
-        }
-        sort(sessions.begin(), sessions.end());
-        maxV = 0, i = 0;
-        while (i < N) {
-            j=1;
-            int k=0;
-            bool flag = 0; 
-            while (k < sessions[i].length() && j<K) {
-                
-            }
-        }
+typedef long long ll;
 
-        cout << "Case #" << (index++) << ": " << maxV << endl;
+const int maxn = 2e6 + 7;
+
+string a[maxn];
+int t[maxn][26];
+int val[maxn];
+int tot;
+
+void insert(string s) {
+    int u = 0;
+    for(int i = 0;i < s.size();i++) {
+        int id = s[i] - 'A';
+        if(!t[u][id]) {
+            t[u][id] = ++tot;
+        }
+        val[t[u][id]]++;
+        u = t[u][id];
+    }
+}
+
+int main() {
+    int T;scanf("%d",&T);
+    int kase = 0;
+    while(T--) {
+        tot = 0;
+        memset(val,0,sizeof(val));
+        memset(t,0,sizeof(t));
+        int n,k;scanf("%d%d",&n,&k);
+        for(int i = 1;i <= n;i++) {
+            cin >> a[i];
+            insert(a[i]);
+        }
+        
+        int ans = 0;
+        for(int i = 1;i <= tot;i++) {
+            ans += val[i] / k;
+        }
+        
+        printf("Case #%d: %d\n",++kase,ans);
     }
     return 0;
 }
