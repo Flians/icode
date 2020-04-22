30616e558d51936077c73ba1e563638406f6d164
dcsgman <rongliangfu@outlook.com>
2020-04-22 16:01:24 +0800

    icode

---
+++ leetcode/160_Intersection_of_Two_Linked_Lists.h
@@ -0,0 +1,35 @@
+#include "../base/icode.h"
+
+class L160 : public icode {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *ca = headA, *cb = headB;
+        int flag=0;
+        while (ca && cb) {
+            if (ca==cb)
+                return ca;
+            
+            if (ca->next)
+                ca = ca->next;
+            else if(flag <2) {
+                ca = headB;
+                ++flag;
+            } else {
+                ca = NULL;
+            }
+            if (cb->next)
+                cb = cb->next;
+            else if(flag <2) {
+                cb = headA;
+                ++flag;
+            } else {
+                cb = NULL;
+            }
+        }
+        return NULL;
+    }
+
+    void run() {
+        
+    }
+};
