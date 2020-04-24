7166a04b3d198c8473f9f504933750cd2c9950aa
dcsgman <rongliangfu@outlook.com>
2020-04-24 16:29:46 +0800

    icode

--- base/icode.h
+++ base/icode.h
@@ -37,6 +37,23 @@ public:
     vector<int> dimensions();
 };
 
+// Definition for a Node.
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+    Node *next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node *_left, Node *_right, Node *_next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
 class icode
 {
 private:
