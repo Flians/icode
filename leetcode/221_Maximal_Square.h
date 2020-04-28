d1a76957fd76a06fd1963fc7eb865b1a10179cfa
dcsgman <rongliangfu@outlook.com>
2020-04-28 09:21:13 +0800

    icode

--- leetcode/221_Maximal_Square.h
+++ leetcode/221_Maximal_Square.h
@@ -2,7 +2,23 @@
 class L221 : public icode
 {
 public:
-    int maximalSquare(vector<vector<char> > &matrix)
+    int maximalSquare(vector<vector<char>>& matrix) {
+        if (matrix.empty())
+            return 0;
+        int i=0,j=0,m=matrix.size(),n=matrix[0].size(), res=0;
+        vector<vector<int> > dp(m+1, vector<int>(n+1,0));
+        for (i=1; i<=m; ++i) {
+            for (j=1; j<=n; ++j) {
+                if (matrix[i-1][j-1]=='1') {
+                    dp[i][j] = min(min(dp[i-1][j], dp[i][j-1]),dp[i-1][j-1]) + 1;
+                    res = max(res, dp[i][j]);
+                }
+            }
+        }
+        return res*res;
+    }
+
+    int maximalSquare_best(vector<vector<char> > &matrix)
     {
         if (matrix.empty() || matrix[0].empty())
             return 0;
@@ -30,5 +46,7 @@ public:
 
     void run()
     {
+        vector<vector<char> > nums = {{'1','0','1','0'},{'1','0','1','1'},{'1','0','1','1'},{'1','1','1','1'}};
+        cout << maximalSquare_best(nums) << endl;
     }
 };
