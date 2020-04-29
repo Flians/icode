d006c0e8ab71538f2bd27ac59b03d3edc2ae8cb2
dcsgman <rongliangfu@outlook.com>
2020-04-29 09:18:06 +0800

    icode

---
+++ leetcode/First_Unique_Number.h
@@ -0,0 +1,54 @@
+#include "../base/icode.h"
+
+class FirstUnique : public icode
+{
+public:
+    list<int> uns;
+    map<int, list<int>::iterator> record;
+    FirstUnique(vector<int> &nums)
+    {
+        for (auto num : nums)
+        {
+            add(num);
+        }
+    }
+
+    int showFirstUnique()
+    {
+        if (uns.empty())
+        {
+            return -1;
+        }
+        else
+        {
+            return uns.front();
+        }
+    }
+
+    void add(int value)
+    {
+        if (record.count(value) == 0)
+        {
+            uns.push_back(value);
+            record[value] = --uns.end();
+        }
+        else if (record[value] != uns.end())
+        {
+            uns.erase(record[value]);
+            record[value] = uns.end();
+        }
+    }
+
+    void run()
+    {
+        vector<int> nums = {2, 3, 5};
+        FirstUnique *obj = new FirstUnique(nums);
+        cout << obj->showFirstUnique() << endl;
+        obj->add(5);
+        cout << obj->showFirstUnique() << endl;
+        obj->add(2);
+        cout << obj->showFirstUnique() << endl;
+        obj->add(3);
+        cout << obj->showFirstUnique() << endl;
+    }
+};
