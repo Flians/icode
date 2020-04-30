d20163ea28cf0c06483ce2a2bf22cf4c7efdfaae
dcsgman <rongliangfu@outlook.com>
2020-04-30 15:37:18 +0800

    icode

--- base/Factory.h
+++ base/Factory.h
@@ -20,6 +20,7 @@
 #include "../leetcode/105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.h"
 #include "../leetcode/106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.h"
 #include "../leetcode/116_Populating_Next_Right_Pointers_in_Each_Node.h"
+#include "../leetcode/124_Binary_Tree_Maximum_Path_Sum.h"
 #include "../leetcode/146_LRU_Cache.h"
 #include "../leetcode/155_Min_Stack.h"
 #include "../leetcode/160_Intersection_of_Two_Linked_Lists.h"
@@ -92,6 +93,8 @@ public:
             return new L106;
         case 116:
             return new L116;
+        case 124:
+            return new L124;
         case 146:
             return new LRUCache;
         case 155:
