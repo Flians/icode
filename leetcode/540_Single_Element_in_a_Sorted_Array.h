e7c0ebcb6955da3c057f04a9efbae1ea9fc591f2
dcsgman <rongliangfu@outlook.com>
2020-05-13 09:14:20 +0800

    icode

---
+++ leetcode/540_Single_Element_in_a_Sorted_Array.h
@@ -0,0 +1,15 @@
+#include "../base/icode.h"
+class L540 : public icode {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int res = 0;
+        for (auto num : nums)
+            res ^= num;
+        return res;
+    }
+
+    void run() {
+        vector<int> nums = {1,1,2,3,3,4,4,8,8};
+        cout << singleNonDuplicate(nums) << endl;
+    }
+};
