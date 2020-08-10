0a405f85e011c96af84425902b3f5f50cdc5f71a
Flians <rongliangfu@outlook.com>
2020-08-10 20:57:40 +0800

    change 987

--- leetcode/987_Vertical_Order_Traversal_of_a_Binary_Tree.hpp
+++ leetcode/987_Vertical_Order_Traversal_of_a_Binary_Tree.hpp
@@ -4,72 +4,32 @@ class L987 : public icode
 public:
 	void run() {}
 
-    /*
-    void verticalTraversal(TreeNode* root, int &index, vector<vector<int>> &res) {
-        if (!root)
-            return;
-        cout << index << endl;
-        if (index == 0) {
-            vector<int> item;
-            item.emplace_back(root->val);
-            res.insert(res.begin(), item);
-            verticalTraversal(root->left, index, res);
-            verticalTraversal(root->right, ++index, res);
-        } else if (index >= res.size()) {
-            vector<int> item;
-            item.emplace_back(root->val);
-            res.emplace_back(item);
-            verticalTraversal(root->left, --index, res);
-            ++index;
-            verticalTraversal(root->right, ++index, res);
-        } else {
-            res[index].emplace_back(root->val);
-            verticalTraversal(root->left, --index, res);
-            ++index;
-            verticalTraversal(root->right, ++index, res);
-        }
-    }
-    
-    vector<vector<int>> verticalTraversal(TreeNode* root) {
-        int index = 0;
-        vector<vector<int> > res;
-        verticalTraversal(root, index, res);
-        return res;
-    }
-    */
-    map<int,vector<pair<int,int>>> m;
-    
-    vector<vector<int>> ans;
-    
-    static bool cmp(pair<int,int>& a,pair<int,int>& b){
-        if(a.second==b.second){
-            return a.first<b.first;
-        }
-        return a.second<b.second;
-    }
-    
-    void rec(TreeNode* cur,int hd,int height){
+    void rec(TreeNode* cur,int hd,int height, map<int,vector<pair<int,int>>> &m){
         m[hd].push_back({(cur->val),height});
         
         if(cur->left!=nullptr){
-            rec(cur->left,hd-1,height+1);
+            rec(cur->left,hd-1,height+1,m);
         }
         
         if(cur->right!=nullptr){
-            rec(cur->right,hd+1,height+1);
+            rec(cur->right,hd+1,height+1,m);
         }
     }
     
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        rec(root,0,0);
+        map<int,vector<pair<int,int>>> m;
+        rec(root,0,0,m);
+        vector<vector<int>> res;
         for(auto it:m){
-            sort(it.second.begin(),it.second.end(),cmp);
-            vector<int> tans;
+            sort(it.second.begin(),it.second.end(),[&](pair<int, int> &a, pair<int, int> &b){
+                return a.second == b.second ? a.first < b.first : a.second < b.second;
+            });
+            vector<int> item;
             for (auto it1:it.second){
-                tans.push_back(it1.first);
+                item.push_back(it1.first);
             }
-            ans.push_back(tans);
+            res.push_back(item);
         }
-        return ans;
+        return res;
     }
 };
