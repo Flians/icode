c008efd74e0d6cb28039c070534db7b94ee753ef
Flians <rongliangfu@outlook.com>
2020-08-10 16:16:36 +0800

    change 987

--- leetcode/987_Vertical_Order_Traversal_of_a_Binary_Tree.hpp
+++ leetcode/987_Vertical_Order_Traversal_of_a_Binary_Tree.hpp
@@ -1,16 +1,9 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
+#include "../base/icode.hpp"
+class L987 : public icode
+{
 public:
+	void run() {}
+
     /*
     void verticalTraversal(TreeNode* root, int &index, vector<vector<int>> &res) {
         if (!root)
