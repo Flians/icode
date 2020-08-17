7fd9d82b5f646965e23f7e40e863e67de21af768
Flians <rongliangfu@outlook.com>
2020-08-17 17:28:13 +0800

    add const *

--- others/company/NetEase/leihuo1.cpp
+++ others/company/NetEase/leihuo1.cpp
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include <utility>
+
+// *操作符是左操作符，左操作符的优先级是从右到左
+// const int *p与int const *p等同，指向一个常量，不能通过指针来修改这个指针指向的值。但是p可以修改
+// const int *p的含义是p是指向常量整形的指针, p不能再重新赋值了，即不能再指向另一个新地址。但是*p可以修改
+// int *const p的含义是p是指向int的常量指针
+
 class A
 {
 public:
