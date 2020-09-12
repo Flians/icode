dcfcfa20c04f9850992cd3dff525db1f082204b3
Flians <rongliangfu@outlook.com>
2020-09-12 13:08:21 +0800

    add 10

--- leetcode/10_Regular_Expression_Matching.hpp
+++ leetcode/10_Regular_Expression_Matching.hpp
@@ -47,6 +47,27 @@ public:
         return dp(0, 0, s, p);
     }
 
+    
+    bool isMatch_dp(string s, string p) {
+        int m = s.size(),n = p.size();
+        vector<vector<bool>> dp(m+1,vector<bool>(n+1,false));
+        dp[0][0] = true;
+        for (int j = 1;j < n && p[j] == '*';j+=2)
+            dp[0][j+1] = true;
+        for (int i = 0;i < m;i++)
+            for (int j = 0;j < n;j++){
+                if (p[j]!='*')
+                    //如果是字母或.只受左上方向影响，即方向3
+                    dp[i+1][j+1] = p[j]=='.' ? dp[i][j] : (dp[i][j] && (p[j]==s[i]));
+                else
+                    //如果是*则受左和上两个方向影响，即方向1和2，有三个或分别对应上述三种情况
+                    // p[j - 1] != s[i] 且 p[j - 1] != '.' -> dp[i+1][j-1] *取0
+                    // p[j - 1] == s[i] 或 p[j - 1] == '.' -> dp[i + 1][j] || dp[i][j + 1] || dp[i + 1][j - 1] 
+                    dp[i+1][j+1] = dp[i+1][j-1] || dp[i+1][j] || (dp[i][j+1]&&((p[j-1]==s[i])||p[j-1]=='.'));
+            }
+        return dp[m][n];
+    }
+
     void run()
     {
         cout << "mississippi - mis*is*p*. : " << isMatch("mississippi", "mis*is*p*.") << endl;
