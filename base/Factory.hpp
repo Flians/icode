a99dcf93d04306ed53ff8e3c17424e54a2d25bc6
Flians <rongliangfu@outlook.com>
2020-09-25 13:14:42 +0800

    add 980

--- base/Factory.hpp
+++ base/Factory.hpp
@@ -151,6 +151,7 @@
 #include "../leetcode/967_Numbers_With_Same_Consecutive_Differences.hpp"
 #include "../leetcode/969_Pancake_Sorting.hpp"
 #include "../leetcode/973_K_Closest_Points_to_Origin.hpp"
+#include "../leetcode/980_Unique_Paths_III.hpp"
 #include "../leetcode/983_Minimum_Cost_For_Tickets.hpp"
 #include "../leetcode/986_Interval_List_Intersections.hpp"
 #include "../leetcode/987_Vertical_Order_Traversal_of_a_Binary_Tree.hpp"
@@ -487,6 +488,8 @@ public:
 			return new L969;
 		case 973:
 			return new L973;
+		case 980:
+			return new L980;
 		case 983:
 			return new L983;
 		case 986:
