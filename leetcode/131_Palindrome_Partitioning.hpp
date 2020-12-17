2d248725824a9d706ee538df14e808c7f90a6fb6
Flians <rongliangfu@outlook.com>
2020-12-17 15:27:30 +0800

    update 131

--- leetcode/131_Palindrome_Partitioning.hpp
+++ leetcode/131_Palindrome_Partitioning.hpp
@@ -10,7 +10,11 @@ public:
     vector<vector<string>> partition(string s) {
         vector<vector<string>> res;
         vector<string> cur;
-        dfs(res, s, 0,  cur);
+        // dfs(res, s, 0,  cur);
+
+        vector<vector<bool>> dp(s.size(), vector<bool>(s.size(), 0));
+        dfs_dp(res, s, 0,  cur, dp);
+        
         return res;
     }
     
@@ -27,12 +31,26 @@ public:
             }
         }
     }
-    
     bool is_palind(string &s, int l, int h) {
         while (l < h) {
             if (s[l++] != s[h--])
                 return false;
         }
         return true;
+    }
+
+    void dfs_dp(vector<vector<string>> &res, string & s, int begin, vector<string> cur, vector<vector<bool>> &dp) {
+        int len = s.size();
+        if (begin >= len) {
+            res.push_back(cur);
+        }
+        for (int end = begin; end < len; ++end) {
+            if (s[begin] == s[end] && (end - begin <= 2 || dp[begin + 1][end - 1])) {
+                dp[begin][end] = true;
+                cur.push_back(s.substr(begin, end-begin+1));
+                dfs_dp(res, s, end + 1,  cur, dp);
+                cur.pop_back();
+            }
+        }
     }
 };
