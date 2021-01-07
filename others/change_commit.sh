7a0c489fbe2ff9cc33a8ec4fc8fec85d03132e42
Flians <rongliangfu@outlook.com>
2021-01-07 16:30:28 +0800

    add script for changing the commits of git

---
+++ others/change_commit.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+
+CORRECT_NAME="Flians"
+CORRECT_EMAIL="rongliangfu@outlook.com"
+
+if [ "$GIT_COMMITTER_EMAIL" != "$CORRECT_NAME" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" != "$CORRECT_NAME" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
