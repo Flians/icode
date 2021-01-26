c1e289ea3012f4fc7080022d90c0acd9fc89eb1c
Flians <rongliangfu@outlook.com>
2021-01-26 13:53:41 +0800

    update 23

--- leetcode/23_Merge_k_Sorted_Lists.hpp
+++ leetcode/23_Merge_k_Sorted_Lists.hpp
@@ -48,16 +48,57 @@ public:
         }
         return flag ? NULL : result;
     }
+
+    ListNode *mergeKLists_(vector<ListNode *> &&lists)
+    {
+        auto cmp = [](pair<ListNode *, int> &a, pair<ListNode *, int> &b) {
+            if (a.first && b.first)
+            {
+                return a.first->val == b.first->val ? a.second > b.second : a.first->val > b.first->val;
+            }
+            else
+            {
+                return a.first == nullptr;
+            }
+        };
+        priority_queue<pair<ListNode *, int>, vector<pair<ListNode *, int>>, decltype(cmp)> record(cmp);
+        size_t k = lists.size();
+        ListNode *root = new ListNode(-1);
+        ListNode *cur = root;
+        //init
+        for (size_t i = 0; i < k; ++i)
+        {
+            if (lists[i])
+            {
+                record.emplace(lists[i], i);
+                lists[i] = lists[i]->next;
+            }
+        }
+        while (!record.empty())
+        {
+            auto tmp = record.top();
+            record.pop();
+            if (tmp.first)
+            {
+                cur->next = tmp.first;
+                cur = tmp.first;
+                if (lists[tmp.second])
+                {
+                    record.emplace(lists[tmp.second], tmp.second);
+                    lists[tmp.second] = lists[tmp.second]->next;
+                }
+            }
+        }
+        cur->next = nullptr;
+        cur = root;
+        root = root->next;
+        delete cur;
+        return root;
+    }
+
     void run()
     {
-        /*
-Input:
-[
-  1->4->5,
-  1->3->4,
-  2->6
-]
-Output: 1->1->2->3->4->4->5->6
-        */
+        //1->1->2->3->4->4->5->6
+        print_res(mergeKLists_({create_list({1, 4, 5}), create_list({1, 3, 4}), create_list({2, 6})}));
     }
 };
