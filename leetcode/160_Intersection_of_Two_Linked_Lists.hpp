be6bac747fcb97be6cbf0a73b4287dfec15fecef
Flians <rongliangfu@outlook.com>
2021-03-16 10:00:15 +0800

    update 160

--- leetcode/160_Intersection_of_Two_Linked_Lists.hpp
+++ leetcode/160_Intersection_of_Two_Linked_Lists.hpp
@@ -1,35 +1,89 @@
 #include "../base/icode.hpp"
 
-class L160 : public icode {
+class L160 : public icode
+{
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
+    {
         ListNode *ca = headA, *cb = headB;
-        int flag=0;
-        while (ca && cb) {
-            if (ca==cb)
+        int flag = 0;
+        while (ca && cb)
+        {
+            if (ca == cb)
                 return ca;
-            
+
             if (ca->next)
                 ca = ca->next;
-            else if(flag <2) {
+            else if (flag < 2)
+            {
                 ca = headB;
                 ++flag;
-            } else {
+            }
+            else
+            {
                 ca = NULL;
             }
             if (cb->next)
                 cb = cb->next;
-            else if(flag <2) {
+            else if (flag < 2)
+            {
                 cb = headA;
                 ++flag;
-            } else {
+            }
+            else
+            {
                 cb = NULL;
             }
         }
         return NULL;
     }
 
-    void run() {
-        
+    ListNode *getIntersectionNode_(ListNode *headA, ListNode *headB)
+    {
+        std::size_t la = 0, lb = 0;
+        ListNode *ta = headA, *tb = headB;
+        while (ta)
+        {
+            ++la;
+            ta = ta->next;
+        }
+        while (tb)
+        {
+            ++lb;
+            tb = tb->next;
+        }
+        if (la < lb)
+        {
+            lb -= la;
+            while (lb--)
+            {
+                headB = headB->next;
+            }
+        }
+        if (la > lb)
+        {
+            la -= lb;
+            while (la--)
+            {
+                headA = headA->next;
+            }
+            la = lb;
+        }
+        while (la--)
+        {
+            if (headA == headB)
+            {
+                return headA;
+            }
+            headA = headA->next;
+            headB = headB->next;
+        }
+        return nullptr;
+    }
+
+    void run()
+    {
+        auto res = getIntersectionNode_(create_list({4, 1, 8, 4, 5}), create_list({5, 6, 1, 8, 4, 5}));
+        std::cout << (res ? res->val : -1) << std::endl;
     }
 };
