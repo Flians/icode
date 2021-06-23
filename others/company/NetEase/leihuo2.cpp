1d674312b0e1bf53904b0d9ecefbd0f3ca3a9b12
Flians <rongliangfu@outlook.com>
2021-06-23 18:58:06 +0800

    add member

--- others/company/NetEase/leihuo2.cpp
+++ others/company/NetEase/leihuo2.cpp
@@ -11,18 +11,21 @@ public:
     int a2 = 20;
     static int a3;
     const int a4 = 40;
-    A() :a2(a3 + 1), a1(a2 + 1) {}
+    int a5 = 50;
+    A() : a2(a3 + 1), a1(a2 + 1) {}
 };
 int A::a3 = 30;
 
 A a;
 
-int main() {
+int main()
+{
 
     cout << a.a1 << endl;
     cout << a.a2 << endl;
     cout << a.a3 << endl;
-
+    cout << a.a4 << endl;
+    cout << a.a5 << endl;
     getchar();
     return 0;
 }
