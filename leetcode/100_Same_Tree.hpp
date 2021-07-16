766d4c5365ffc6f8c758d1b68695fe3a31dbfea1
Flians <rongliangfu@outlook.com>
2021-07-16 11:35:20 +0800

    add 100

---
+++ leetcode/100_Same_Tree.hpp
@@ -0,0 +1,18 @@
+#include "../base/icode.hpp"
+class L100 : public icode
+{
+public:
+    void run()
+    {
+        std::cout << isSameTree(create_tree({1, 2, 3, -1, -1, 4, 5}), create_tree({1, 2, 3})) << " False" << std::endl;
+    }
+
+    bool isSameTree(TreeNode *A, TreeNode *B)
+    {
+        if (!A && !B)
+            return true;
+        if (!A || !B)
+            return false;
+        return A->val == B->val && isSameTree(A->left, B->left) && isSameTree(A->right, B->right);
+    }
+};
