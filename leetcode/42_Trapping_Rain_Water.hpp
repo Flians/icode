42e862cd9b3ad5ea13c3a912f921c04eabdbbcf4
Flians <rongliangfu@outlook.com>
2021-08-06 11:12:19 +0800

    update 877

--- leetcode/42_Trapping_Rain_Water.hpp
+++ leetcode/42_Trapping_Rain_Water.hpp
@@ -2,45 +2,61 @@
 class L42 : public icode
 {
 public:
-    void run() {
-    	cout << trap({0,1,0,2,1,0,1,3,2,1,2,1}) << " 6\n";
+    void run()
+    {
+        cout << trap({0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}) << " 6\n";
     }
 
-    int trap_dp(vector<int>& height) {
+    int trap_dp(vector<int> &height)
+    {
         if (height.empty())
             return 0;
-        int i=0, temp=0, sum=0;
-        vector<int> l(height.size()), r(height.size());
-        
-        for(; i<height.size(); i++) {
+        int i = 0, temp = 0, sum = 0, n = height.size();
+        vector<int> l(n), r(n);
+
+        for (; i < n; i++)
+        {
             temp = max(temp, height[i]);
             l[i] = temp;
         }
         temp = 0;
-        for(i=height.size()-1; i>=0; i--) {
+        for (i = n - 1; i >= 0; i--)
+        {
             temp = max(temp, height[i]);
             r[i] = temp;
         }
-        for(i=0; i<height.size(); i++) {
+        for (i = 0; i < n; i++)
+        {
             sum += min(l[i], r[i]) - height[i];
         }
         return sum;
     }
-    
-    int trap(vector<int> &&height) {
-        int l = 0, r = height.size()-1;
+
+    int trap(vector<int> &&height)
+    {
+        int l = 0, r = height.size() - 1;
         int res = 0, lmax = 0, rmax = 0;
-        while (l < r) {
-            if (height[l] < height[r]) {
-                if (height[l] >= lmax) {
+        while (l < r)
+        {
+            if (height[l] < height[r])
+            {
+                if (height[l] >= lmax)
+                {
                     lmax = height[l++];
-                } else {
+                }
+                else
+                {
                     res += lmax - height[l++];
                 }
-            } else {
-                if (height[r] >= rmax) {
+            }
+            else
+            {
+                if (height[r] >= rmax)
+                {
                     rmax = height[r--];
-                } else {
+                }
+                else
+                {
                     res += rmax - height[r--];
                 }
             }
--- leetcode/877_Stone_Game.hpp
+++ leetcode/877_Stone_Game.hpp
@@ -2,9 +2,26 @@
 class L877 : public icode
 {
 public:
-	void run() {}
+    void run()
+    {
+        cout << stoneGame({5, 3, 4, 5}) << " true\n";
+    }
 
-    bool stoneGame(vector<int>& piles) {
-        return true;
+    bool stoneGame(vector<int> &&piles)
+    {
+        int n = piles.size();
+        vector<vector<int>> dp(n, vector<int>(n));
+        for (int i = 0; i < n; ++i)
+            dp[i][i] = piles[i];
+        for (int len = 1; len < n; ++len)
+        {
+            for (int i = 0; i < n - len; ++i)
+            {
+                int j = i + len;
+                dp[i][j] = max(piles[i] - dp[i + 1][j], piles[j] - dp[i][j - 1]);
+            }
+        }
+        return dp[0][n - 1] > 0;
+        // return true;
     }
 };
