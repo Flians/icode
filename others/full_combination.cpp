9bb0e6e2b34324db441940a388769a02bc5c6776
Flians <rongliangfu@outlook.com>
2022-03-15 15:50:25 +0800

    update iterate full combination without limitation

--- others/full_combination.cpp
+++ others/full_combination.cpp
@@ -2,16 +2,17 @@
 #include <vector>
 #include <string>
 
-void Combination(int m, int index, int sub_size, std::vector<char> &leaves, std::string &result);
+void Combination(size_t m, size_t index, size_t sub_size, std::string &result, std::vector<char> &leaves);
 
 void Combination1(std::vector<char> &leaves)
 {
     std::string result;
     for (size_t i = 1, length = leaves.size(); i <= length; i++)
-        Combination(i, 0, i, leaves, result);
+        Combination(i, 0, i, result, leaves);
+    std::cout << result << std::endl;
 }
 
-void Combination(int m, int index, int sub_size, std::vector<char> &leaves, std::string &result)
+void Combination(size_t m, size_t index, size_t sub_size, std::string &result, std::vector<char> &leaves)
 {
     if (m == 0)
     {
@@ -21,35 +22,53 @@ void Combination(int m, int index, int sub_size, std::vector<char> &leaves, std:
     if (index + m > leaves.size())
         return;
     result += leaves[index];
-    Combination(m - 1, index + 1, sub_size, leaves, result);
+    Combination(m - 1, index + 1, sub_size, result, leaves);
     if (sub_size == leaves.size())
         return;
     result.pop_back();
-    Combination(m, index + 1, sub_size, leaves, result);
+    Combination(m, index + 1, sub_size, result, leaves);
 }
 
 void Combination2(std::vector<char> &leaves)
 {
     size_t num_leaf = leaves.size();
-    uint64_t nBit = 1 << num_leaf;
-    for (uint64_t i = 1; i < nBit; i++)
+    std::string nBit_str(num_leaf, '0');
+    nBit_str.append("1");
+    std::string cur = "1";
+    cur.append(num_leaf, '0');
+    size_t threshold = 1;
+    while (cur != nBit_str)
     {
-        size_t k = 0;
-        for (size_t j = 0; j < num_leaf; j++)
+        size_t k = 0, j = 0;
+        for (j = 0; j < threshold; j++)
         {
-            if (i & (1 << j))
+            if (cur[j] == '1')
             {
                 k++;
                 std::cout << leaves[j];
             }
         }
-        std::cout << "; " << k << " -> " << i << std::endl;
+        std::cout << "; " << k << " -> " << cur << std::endl;
+        for (j = 0; j <= threshold; ++j)
+        {
+            if (cur[j] == '1')
+            {
+                cur[j] -= 1;
+            }
+            else
+            {
+                cur[j] += 1;
+                break;
+            }
+        }
+        if (j == threshold)
+            ++threshold;
     }
 }
 
 int main()
 {
-    std::vector<char> leaves = {'A', 'B', 'C', 'D'};
+    std::vector<char> leaves = {'A', 'B', 'C', 'D', 'E'};
     Combination2(leaves);
     return 0;
 }
